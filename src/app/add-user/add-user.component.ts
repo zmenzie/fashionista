47b768615860f8d6b34edbbcffe146e107f0f0c6
zmenzie <zmenzie@purdue.edu>
2020-11-19 17:24:42 -0500

    User list now has add-user functionality

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  formData: any = {};
+  errors: any = [];
+
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  register(): void {
+    //console.log(this.formData);
+    this.errors = [];
+    this.auth.register(this.formData)
+      .subscribe(() => {
+        this.router.navigate(['/users-list'], { queryParams: { registered: 'success' } });
+      },
+        (errorResponse) => {
+          this.errors.push(errorResponse.error.error);
+        });
+  }
 }
